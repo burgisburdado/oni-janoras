bb779abdab62cf1ee0051002b3eacb81e7792012
Oshni Janoras <oshnijanoras@192.168.1.7>
2024-12-16 16:48:28 +0800

    adds sidebar

---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Clapperboard, FileImage, Music, User, Workflow } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Work",
+    url: "#",
+    icon: Workflow,
+  },
+  {
+    title: "Videos",
+    url: "#",
+    icon: Clapperboard,
+  },
+  {
+    title: "Graphics",
+    url: "#",
+    icon: FileImage,
+  },
+  {
+    title: "Music",
+    url: "#",
+    icon: Music,
+  },
+  {
+    title: "About",
+    url: "#",
+    icon: User,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>ONI JANORAS</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
